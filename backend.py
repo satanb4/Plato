474eea9c1b63e8c8f1a38b7b76ce6987a6200c58
Sayan97 <sayan.bandyopadhyay@yahoo.co.in>
2019-03-24 03:16:48 +0530

    backend fix

--- backend.py
+++ backend.py
@@ -1,96 +1,81 @@
-from flask import Flask 
 import sqlite3 as sq
-from flask_bcrypt import Bcrypt
-
-app = Flask(__name__)
-bcrypt = Bcrypt(app)
 
 class Database:
 
     def __init__(self):
-        global conn
-        global cur
-        conn = sq.connect(r"user_data.db")
-        cur = conn.cursor()
-        cur.executescript(
-            "CREATE TABLE IF NOT EXISTS users(sid INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,fname TEXT,sname TEXT,mail TEXT,password VARCHAR); \
-            CREATE TABLE IF NOT EXISTS academics(aid INTEGER,class INTEGER,school TEXT,city TEXT, PRIMARY KEY(aid),FOREIGN KEY(aid) REFERENCES users(sid) ON DELETE CASCADE); \
-            CREATE TABLE IF NOT EXISTS achievements(uid INTEGER,points INTEGER, level INTEGER,PRIMARY KEY(uid),FOREIGN KEY(uid) REFERENCES users(sid) ON DELETE CASCADE);\
-            CREATE TABLE IF NOT EXISTS topics(tid INTEGER PRIMARY KEY, topic TEXT)"
+        self.conn = sq.connect("user")
+        self.cur = self.conn.cursor()
+        self.cur.execute(
+            "CREATE TABLE IF NOT EXISTS users(sid INTEGER PRIMARY KEY,fname TEXT,sname TEXT,mail TEXT,password VARCHAR);"
+        )
+        self.cur.execute(
+            "CREATE TABLE IF NOT EXISTS academics(class INTEGER,school TEXT,city TEXT,aid INTEGER, FOREIGN KEY(aid) REFERENCES users(sid) ON DELETE CASCADE);"
+        )
+        self.cur.execute(
+            "CREATE TABLE IF NOT EXISTS achievements(points INTEGER, level INTEGER,uid INTEGER, FOREIGN KEY(uid) REFERENCES users(sid) ON DELETE CASCADE);"
         )
         """
         cur.execute(
             "CREATE TABLE IF NOT EXISTS academics(sid INTEGER FOREIGN KEY(sid) REFERENCES users(sid),class INT,school TEXT,city TEXT)"
         )"""
-        conn.commit()
+        self.conn.commit()
     
     def add_user(self,fname,sname,mail,password):
-        error = None
-        cur.execute(
+        self.cur.execute(
             "INSERT INTO users VALUES(NULL,?,?,?,?)",(fname,sname,mail,password)
         )
-        conn.commit()
-        if error != None:
-            conn.rollback()
+        self.conn.commit()
     
     def add_academics(self,classs,school,city):
-        error = None
-        cur.execute(
+        self.cur.execute(
             "INSERT INTO academics VALUES(NULL,?,?,?)",(classs,school,city)
         )
-        conn.commit()
-        if error != None:
-            conn.rollback()
+        self.conn.commit()
     
     def update_users(self,sid,fname,sname):
-        error = None
-        cur.execute(
+        self.cur.execute(
             "UPDATE academics SET fname=?,sname=? WHERE sid=?",(fname,sname,sid)
         )
-        conn.commit()
-        if error != None:
-            conn.rollback()
+        self.conn.commit()
 
-    def update_academics(self,sid,classs,school,city):
-        error = None
-        cur.execute(
+    def update_academics(self,sid,classs,school="",city=""):
+        self.cur.execute(
             "UPDATE academics SET class=?,school=?,city=? WHERE sid=?",(classs,school,city,sid)
         )
-        conn.commit()
-        if error != None:
-            conn.rollback()
+        self.conn.commit()
     
     def update_achievements(self,sid,points,level):
-        error = None
-        
-        cur.execute(
+        self.cur.execute(
             "UPDATE achievements SET points=?,level=? WHERE sid=?",(points,level,sid)
         )
-        conn.commit()
-        if error != None:
-            conn.rollback()
+        self.conn.commit()
+
 
     def view_users(self,sid):
-        cur.execute(
-            "SELECT fname,sname,birthday,mail FROM users WHERE sid=?",(sid)
+        self.cur.execute(
+            "SELECT fname,sname,mail,password FROM users WHERE sid=?",(sid)
         )
-        rows = cur.fetchall()
+        rows = self.cur.fetchall()[0]
         return rows
     
-    def login_check(self,mail):
-        cur.execute(
-            "SELECT * FROM users WHERE mail=?",(mail)
+    def id_of_mail(self,mail=""):
+        self.cur.execute(
+            "SELECT sid FROM users WHERE mail=?",(mail,)
         )
-        rows = cur.fetchall()
+        rows = self.cur.fetchall()[0]
         return rows
-
     
     def view_academics(self,sid):
-        cur.execute(
+        self.cur.execute(
             "SELECT * FROM academics WHERE sid=?",(sid)
         )
-        rows = cur.fetchall()
+        rows = self.cur.fetchall()[0]
         return rows
     
+    def delete_user(self,sid):
+        self.cur.execute(
+            "DELETE FROM users WHERE sid=?",(sid)
+        )
+
     def __del__(self):
-        conn.close()
+        self.conn.close()
--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, request, session
 from backend import Database
-#from flask-socketio import SocketIO 
+# from flask-socketio import SocketIO
 import random
 from flask_bcrypt import Bcrypt
 
@@ -10,41 +10,47 @@ app.config['SECRET_KEY'] = "JajtuBhp25@nfoioet"
 # socketio = SocketIO(app)
 bcrypt = Bcrypt(app)
 
+
 @app.route("/")
 def home():
-        lwe = ["jlfsd","jlsdaf","jerwuuefbc"]
-        return render_template("home.html",username="sayan",topics=lwe)
+    lwe = ["jlfsd", "jlsdaf", "jerwuuefbc"]
+    return render_template("home.html", username="sayan", topics=lwe)
+
 
 @app.route("/register")
 def register():
-        return render_template("signup.html")
+    return render_template("signup.html")
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-	db = Database()
-	if request.method == 'POST':
-		dbmail = request.form.get('mail')
-		rows = db.login_check(dbmail)
-		if rows[4] == request.form.get('password'):
-			session['logged_in']=True
-			return render_template("layout.html")
-		else:
-			return render_template("login.html")
-	else:
-		return render_template("login.html")
+    db = Database()
+    if request.method == 'POST':
+        dbmail = request.form.get('mail')
+        rows = db.login_check(dbmail)
+        print(rows)
+        if rows[3] == request.form.get('pass'):
+            session['logged_in'] = True
+            return render_template("layout.html")
+        else:
+            return render_template("login.html")
+    else:
+        return render_template("login.html")
 
-@app.route("/signup", methods=["GET","POST"])
+
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
-	db = Database()
-	if request.method == 'POST':
-		firstname = request.form.get('fname')
-		lastname = request.form.get('sname')
-		email = request.form.get('mail')
-		pword = request.form.get('password')
-		db.add_user(firstname,lastname,email,pword)
-		return render_template("home.html")
-	else:
-		return("signup.html")
+    db = Database()
+    if request.method == 'POST':
+        firstname = request.form.get('fname')
+        lastname = request.form.get('sname')
+        email = request.form.get('mail')
+        pword = request.form.get('password')
+        db.add_user(firstname, lastname, email, pword)
+        return render_template("home.html")
+    else:
+        return("signup.html")
+
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True)
