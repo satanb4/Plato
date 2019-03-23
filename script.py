04a3138680326898751914e1da2a777c8b0ce5a8
Neha <nandineha09@gmail.com>
2019-03-23 13:22:19 +0530

    frontend

--- script.py
+++ script.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, redirect, url_for, request
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="/static")
 
 @app.route("/")
 def home():
-        
+        return render_template("login.html")
+
+if __name__ == "__main__":
+    app.run(debug = True)
