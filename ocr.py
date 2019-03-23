847ee92a38134293d4cd12b39231490fea57b97f
Rushikesh <rushikeshdeotale27@gmail.com>
2019-03-23 17:22:20 +0530

    keyword

--- ocr.py
+++ ocr.py
@@ -3,11 +3,16 @@ import pytesseract
 import argparse
 import cv2
 import os
+from rake_nltk import Rake
 
+r = Rake()
 def ocr():
-    image=cv2.imread('/Users/rushikeshdeotale/Desktop/Project/xy.jpg')
+    image=cv2.imread('/Users/rushikeshdeotale/Desktop/Plato/qw.jpg')
     #gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     text = pytesseract.image_to_string((cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)))
     return text
-print(ocr())
-print(type(ocr()))
+text = ocr()
+#l = (text.split('?'))
+print(text)
+r.extract_keywords_from_text(text)
+print(r.get_ranked_phrases()) 
