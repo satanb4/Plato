4e51dcb8bf020a83d165526bf1f41a88cb56a293
Sayan97 <sayan.bandyopadhyay@yahoo.co.in>
2019-03-23 17:27:07 +0530

    login

--- script.py
+++ script.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, redirect, url_for, request
+from flask_socketio import SocketIO
+import random
+
 
 app = Flask(__name__, static_url_path="/static")
+app.config['SECRET_KEY'] = "JajtuBhp25@nfoioet"
+# socketio = SocketIO(app)
 
 @app.route("/")
 def home():
@@ -9,10 +14,14 @@ def home():
 @app.route("/forgot")
 def forgot():
         return render_template("forgot.html")
-        
+
 @app.route("/signup")
 def signup():
         return render_template("signup.html")
+
+@app.route("/quiz")
+def quiz():
+        return render_template("quiz.html")
 
 if __name__ == "__main__":
     app.run(debug = True)
